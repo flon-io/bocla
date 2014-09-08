bebab31e09bbb3f289d4807659855536b1951f98
John Mettraux <jmettraux@gmail.com>
2014-09-09 06:11:36 +0900

    forgot about the intermediate bash process

--- spec/server.c
+++ spec/server.c
@@ -5,15 +5,25 @@
 // Sun Sep  7 19:27:55 JST 2014
 //
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 
 #include "server.h"
 
 
+int server_ready = 0;
 pid_t server_pid = -1;
+
+void handle_usr1(int signal)
+{
+  printf(". server ready\n");
+  server_ready = 1;
+}
 
 
 void serve()
@@ -24,18 +34,25 @@ void serve()
 
   printf("############# pid: %i\n", server_pid);
 
-  if (server_pid != 0) return;
-  //if (server_pid != 0)
-  //{
-  //  int status;
-  //  wait(&status);
-  //  printf("child exited with status %i\n", status);
-  //  exit(1);
-  //}
+  if (server_pid == 0)
+  {
+    execl("/bin/sh", "", "../spec/server.sh", NULL);
 
-  execl("/bin/sh", "", "../spec/server.sh", NULL);
+    perror("execl failed"); exit(1);
+  }
+  else
+  {
+    struct sigaction a;
+    a.sa_handler = &handle_usr1;
+    int r = sigaction(SIGUSR1, &a, NULL);
 
-  perror("execl failed"); exit(1);
+    if (r < 0) { perror("error setting USR1 handler"); exit(1); }
+
+    while (1)
+    {
+      sleep(1); if (server_ready) { sleep(1); break; }
+    }
+  }
 }
 
 void unserve()
--- spec/server/server.rb
+++ spec/server/server.rb
@@ -6,3 +6,14 @@ get '/' do
   'hello world'
 end
 
+
+# signal parent process that we are [almost] ready
+
+s = `ps | grep "^ *#{Process.ppid}"`.strip
+
+unless s.match(/ bash$/)
+
+  puts "#{Process.pid} signalling #{Process.ppid}..."
+  Process.kill('USR1', Process.ppid)
+end
+
